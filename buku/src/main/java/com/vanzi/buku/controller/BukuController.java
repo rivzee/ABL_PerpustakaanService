3889d873604e2196c58e1ccb1b22b12c76cdce43
Rivzee <rivanzihidayatullah22@gmail.com>
2026-01-19 14:51:16 +0700

    log sukses

--- buku/src/main/java/com/vanzi/buku/controller/BukuController.java
+++ buku/src/main/java/com/vanzi/buku/controller/BukuController.java
@@ -42,10 +42,10 @@ public class BukuController {
         log.info("Request received", kv("action", "GET_BY_ID"), kv("id", id));
         BukuModel buku = bukuService.getBukuById(id);
         if (buku != null) {
-            log.info("Request completed", kv("action", "GET_BY_ID"), kv("status", "SUCCESS"), kv("id", id));
+            log.info("Request completed", kv("action", "GET_BY_ID"), kv("status", "sukses"), kv("id", id));
             return ResponseEntity.ok(buku);
         } else {
-            log.warn("Request completed", kv("action", "GET_BY_ID"), kv("status", "FAILED"), kv("id", id));
+            log.warn("Request completed", kv("action", "GET_BY_ID"), kv("status", "gagal"), kv("id", id));
             return ResponseEntity.notFound().build();
         }
     }
